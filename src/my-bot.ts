39945ce97967140062c09038af9ba8686fbc998c
Mary Bescuca <Mary.Bescuca@softwire.com>
2017-07-20 17:03:49 +0100

    possible improvement 5

--- src/my-bot.ts
+++ src/my-bot.ts
@@ -81,13 +81,13 @@ export class MyBot {
                 rowIndex = nextRowIndex;
                 var newPos = {Row: this.convertToChar(nextRowIndex), Column: nextColumn };
                 var positionString = this.getStringFromPosition(newPos);
-                if(this.triedMap[positionString] == undefined && this.hasTwoNeighbours(newPos)) {
+                if(!this.triedMap[positionString] && this.hasTwoNeighbours(newPos)) {
                     return newPos;
                 }
             }
             var newPos = {Row: this.convertToChar(rowIndex), Column: column };
             var positionString = this.getStringFromPosition(newPos);
-            while(this.triedMap[positionString] != undefined){
+            while(this.triedMap[positionString]){
                 nextColumn = column + 2;
                 nextRowIndex = rowIndex;
                 if(nextColumn == 11) {
