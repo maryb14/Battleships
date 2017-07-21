b9098f9be00010f36bbf0df77f77e91eb3d48369
Mary Bescuca <Mary.Bescuca@softwire.com>
2017-07-21 14:59:02 +0100

    attempt to fix bug

--- src/secondStrategy.ts
+++ src/secondStrategy.ts
@@ -3,16 +3,16 @@ import {Position} from './position'
 export class SecondStrategy {
     constructor() {}
     public getNextTarget(pos: Position, triedMap, hitMap): Position  {
-        var iterPos = pos.getNextPosition();
-        while(iterPos.row != pos.row || iterPos.column != pos.column) {
+        var iterPos = new Position("A", 1);
+        while(iterPos.row != "J" || iterPos.column != 10) {
             var positionString = iterPos.getString();
             if(!triedMap[positionString] && iterPos.hasTwoNeighbours(hitMap)){
                 return iterPos;
             }
             iterPos = iterPos.getNextPosition();
         }
-        var iterPos = pos.getNextPosition();
-        while(iterPos.row != pos.row || iterPos.column != pos.column) {
+        var iterPos = new Position("A", 1);
+        while(iterPos.row != "J" || iterPos.column != 10) {
             var positionString = iterPos.getString();
             if(!triedMap[positionString] && iterPos.hasOneNeighbour(hitMap)){
                 return iterPos;
@@ -23,20 +23,20 @@ export class SecondStrategy {
         var positionString = iterPos.getString();
         var passedTrough : number = 0;
         while(triedMap[positionString] && passedTrough <= 100) {
-            passedTrough++;
-            var nextColumn = iterPos.column + 3;
+            passedTrough;
+            var nextColumn = iterPos.column  3;
             var nextRowIndex = iterPos.row.charCodeAt(0) - 64;
             if(nextColumn === 11) {
                 nextColumn = 3;
-                nextRowIndex = nextRowIndex % 10 + 1;
+                nextRowIndex = nextRowIndex % 10  1;
             }
             if(nextColumn === 12) {
                 nextColumn = 1;
-                nextRowIndex = nextRowIndex % 10 + 1;
+                nextRowIndex = nextRowIndex % 10  1;
             }
             if(nextColumn === 13) {
                 nextColumn = 2;
-                nextRowIndex = nextRowIndex % 10 + 1;
+                nextRowIndex = nextRowIndex % 10  1;
             }
             iterPos = new Position(Position.convertToChar(nextRowIndex), nextColumn);
             var positionString = iterPos.getString();  
